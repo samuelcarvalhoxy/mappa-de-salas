189d8fa404e91da104d0e0cab48c9e7735d3551f
samuelcarvalhoxy <samelucascarvalho@gmail.com>
2026-08-11 19:55:40 -0300

    Hide booking action while mobile menu is open

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -1045,7 +1045,7 @@ export function AppShell() {
           {toast}
         </div>
       )}
-      {(canBookDirectly || canRequest) && (
+      {!sidebar && (canBookDirectly || canRequest) && (
         <button
           className="agenda-fab"
           title="Abrir agenda"
